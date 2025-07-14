48bca95c810a278acc8867e816dff431e51ec94b
siddhartha2457-developer <siddhartha543@gmail.com>
2025-07-14 14:44:49 +0530

    Update server.js
    cors update

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/api/", limiter)
 // )
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173','http://localhost:5000'], // Add your frontend URLs
+  origin: ['http://localhost:3000', 'http://localhost:5173','http://localhost:5000' ,'https://tailorix.in/'], // Add your frontend URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
